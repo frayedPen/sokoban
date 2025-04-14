eebcf6123111ac0d2bb971e3b84e8133e61a5eb0
frayedPen <buldan.556@gmail.com>
2025-04-14 21:12:16 +0400

    Created data structures

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,8 +2,79 @@
 
 using namespace std;
 
+const char LAYOUT[9][8] = {
+	{' ',' ','#','#','#','#','#',' '},
+	{'#','#','#',' ',' ',' ','#',' '},
+	{'#','+',' ','O',' ',' ','#',' '},
+	{'#','#','#',' ','O','+','#',' '},
+	{'#','+','#','#','O',' ','#',' '},
+	{'#',' ','#',' ','+',' ','#','#'},
+	{'#','O',' ','0','O','O','+','#'},
+	{'#',' ',' ',' ','+',' ',' ','#'},
+	{'#','#','#','#','#','#','#','#'}
+};
+
+const char WALL = '#';
+const char BOX = 'O';
+const char SPOT = '+';
+const char ON_SPOT = '0';
+const char SPACE = ' ';
+const char PLAYER = 'P';
+const char INVALID = '!';
+
+const int H = 9;
+const int W = 8;
+
+struct vector2 
+{
+	int x;
+	int y;
+};
+
+class Game 
+{
+	public:
+		struct vector2 player;
+		char level[9][8];
+		void renderResponse();
+		void reset();
+		Game() {
+			reset();
+		};
+};
+
+void Game::renderResponse() 
+{
+	for(int i=0;i<H;i++) {
+		for(int j=0;j<W;j++) {
+			cout << level[i][j];
+        }
+		cout << endl;
+	}
+
+	cout << end1;
+}
+
+void Game::reset() 
+{
+	player.x = 2;
+	player.y = 2;
+
+	for(int i=0;i<H;i++) {
+		for(int j=0;j<W;j++) {
+			level[i][j] = LAYOUT[i][j];
+		}
+	}
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    Game game;
+	string inputStr = "";
+
+	cout << "Type w,a,s,d to move. Can type multiple characters to move. Write reset to restart." << endl;
+
+    cout << game.level[1][1];
+
     return 0;
 }
